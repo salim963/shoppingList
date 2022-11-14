872d33e1572e5338b0b97935b2870c9d211b36ec
Muhammad Bangash <s092512@student.dtu.dk>
2022-11-14 09:53:50 +0100

    homescreen changes

--- app/src/main/java/com/example/shoppinglist/screen/home.kt
+++ app/src/main/java/com/example/shoppinglist/screen/home.kt
@@ -1,8 +1,8 @@
 package com.example.shoppinglist.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -11,26 +11,60 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen() {
-    Box(
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Home Screen",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Create List")
+        }
+    }
+}
+
+/*@Composable
+fun CreateNewList_btn() {
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Magenta),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "HOME",
-            fontSize = MaterialTheme.typography.h3.fontSize,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
+        Button(
+            onClick = { *//*TODO*//* },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Create",
+                fontSize = MaterialTheme.typography.h3.fontSize,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
     }
-}
+
+    }*/
+
 
 @Composable
 @Preview
 fun HomeScreenPreview() {
     HomeScreen()
+    //CreateNewList_btn()
 }
